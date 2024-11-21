69847d4c60739531c177bfa53540cc4ff4a3f776
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-11-21 19:21:32 +0600

    add simple about me

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body
         className={`${jetBrainsMono.variable} antialiased relative bg-background`}
       >
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:24px_24px]"></div>
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:16px_16px]"></div>
         <Providers>{children}</Providers>
       </body>
     </html>
--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -6,8 +6,25 @@ export default function About() {
   const ref = useHashSync('#about');
 
   return (
-    <section ref={ref} id="about" className="w-full h-96 scroll-mt-16">
-      <h1 className="text-center p-8 text-4xl font-bold">About</h1>
+    <section ref={ref} id="about" className="w-full h-screen scroll-mt-16 ">
+      <div className="container py-12">
+        <h1 className="text-center text-white text-5xl font-bold mb-8">
+          About Me
+        </h1>
+        <p className="text-center text-white text-xl mb-4">
+          I&apos;m Saif, a passionate software developer with a love for
+          creating innovative solutions.
+        </p>
+        <p className="text-center text-white text-xl mb-8">
+          With a strong foundation in computer science and a keen eye for
+          design, I craft user-friendly experiences that bring ideas to life.
+        </p>
+        <div className="flex justify-center">
+          <button className="bg-white text-primary-500 hover:bg-primary-500 hover:text-white transition duration-300 ease-in-out py-2 px-4 rounded-full text-lg font-bold">
+            Learn More
+          </button>
+        </div>
+      </div>
     </section>
   );
 }
